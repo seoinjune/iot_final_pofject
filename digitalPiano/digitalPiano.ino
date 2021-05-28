4ba5e29154fa1b25e3079058e169fde8346c884c
seoinjune <sij9323@gmail.com>
2021-05-29 05:11:54 +0900

    lcd제어 기본틀

--- digitalPiano/digitalPiano.ino
+++ digitalPiano/digitalPiano.ino
@@ -1,29 +1,52 @@
 //Digital Piano Project
 
-#include <ESP8266WiFi.h>
+#define LCD_I2C 0x27
 
+#include <ESP8266WiFi.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(LCD_I2C, 16, 2);  
+
+int Lcd_control =0;
 
 void setup() {
 	Serial.begin(74880);
 	delay(1000);
+	//WiFi
 	WiFi.mode(WIFI_AP_STA);
 	WiFi.begin("KT_GiGA_2G_Wave2_80B5","987654321a"); //non-block : WiFi 접속했는지 확인안해
 	WiFi.setAutoReconnect(1);
 	int connectResult = WiFi.waitForConnectResult(); //따라서 기다려줘야해
 	while(1){
 		if(connectResult == WL_CONNECTED){
-		Serial.printf("Connection OK!\r\n");
-		break;    }
-    else{
+			Serial.printf("Connection OK!\r\n");
+			break;    }
+		else{
 		delay(100); 
 		Serial.printf("Connection Failed, Reason=%d",connectResult);
 		}
 	} 
+  
+  //LCD
+  lcd.init();                 
+  lcd.backlight();
 }  
+
+
+void Lcd_print(){
+	if(Lcd_control==0){
+		lcd.setCursor(0, 0);
+		lcd.print("Digital Piano");
+		lcd.setCursor(0,1); 
+	}
+	else if(Lcd_control==1){
+		
+	}
+}
+
+
 
 void loop(){
 
 }
 
-//확인 완료(서호)
-//확인완료 (인준)
